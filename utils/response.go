57bd4d24e024a83ccb5a169ccc29ec7c35fea753
bond95 <cyberbond95@gmail.com>
2019-12-05 13:21:26 +0100

    Added json/application header by default

--- utils/response.go
+++ utils/response.go
@@ -10,13 +10,8 @@ func BuildResponse(status string) map[string]interface{} {
 	return map[string]interface{}{"status": status}
 }
 
-func addJSONHeader(w http.ResponseWriter) {
-	w.Header().Add("Content-Type", "application/json; charset=utf-8")
-}
-
 // SendResponse - return JSON response
 func SendResponse(w http.ResponseWriter, data map[string]interface{}) {
-	addJSONHeader(w)
 	json.NewEncoder(w).Encode(data)
 }
 
