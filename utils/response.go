0916ee20996e54601764e73b373b52bfe99a055e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-12 18:03:40 +0100

    Updated docstrings a bit

--- utils/response.go
+++ utils/response.go
@@ -1,3 +1,21 @@
+// Utils for REST API
+
+/*
+Copyright © 2019 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package utils
 
 import (
@@ -5,29 +23,29 @@ import (
 	"net/http"
 )
 
-// BuildResponse - build response for RestAPI request
+// BuildResponse builds response for RestAPI request
 func BuildResponse(status string) map[string]interface{} {
 	return map[string]interface{}{"status": status}
 }
 
-// SendResponse - return JSON response
+// SendResponse returns JSON response
 func SendResponse(w http.ResponseWriter, data map[string]interface{}) {
 	json.NewEncoder(w).Encode(data)
 }
 
-// SendError - return error response
+// SendError returns error response
 func SendError(w http.ResponseWriter, data map[string]interface{}) {
 	w.WriteHeader(http.StatusBadRequest)
 	SendResponse(w, data)
 }
 
-// SendForbidden - return response with status Forbidden
+// SendForbidden returns response with HTTP status Forbidden
 func SendForbidden(w http.ResponseWriter, data map[string]interface{}) {
 	w.WriteHeader(http.StatusForbidden)
 	SendResponse(w, data)
 }
 
-// SendUnauthorized - return error response
+// SendUnauthorized returns error response for unauthorized access
 func SendUnauthorized(w http.ResponseWriter, data map[string]interface{}) {
 	w.WriteHeader(http.StatusUnauthorized)
 	SendResponse(w, data)
