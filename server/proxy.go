fec3f792fc6d1df7f6b5922d5a85690543bda41f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-19 12:49:21 +0100

    Link to generated documentation

--- server/proxy.go
+++ server/proxy.go
@@ -16,6 +16,9 @@ limitations under the License.
 
 package server
 
+// Documentation in literate-programming-style is available at:
+// https://redhatinsights.github.io/insights-operator-ldapauth/packages/server/proxy.html
+
 import (
 	"io"
 	"log"
