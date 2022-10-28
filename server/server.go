2e3505426ce17123ceb5807374379103ce304fa3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-28 13:02:45 +0200

    Fixed huge param issue

--- server/server.go
+++ server/server.go
@@ -67,7 +67,7 @@ func logRequest(nextHandler http.Handler) http.Handler {
 		})
 }
 
-func (s Server) addDefaultHeaders(nextHandler http.Handler) http.Handler {
+func (s *Server) addDefaultHeaders(nextHandler http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(writer http.ResponseWriter, request *http.Request) {
 			writer.Header().Set("Content-Type", "application/json; charset=utf-8")
